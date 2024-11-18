aff8de182e8747720e4dd63a04f8cc717e242cb6
H-Boit <boitet_hugo@hotmail.com>
2024-11-18 09:34:33 +0100

    Update fonctions_bases.c

--- fonctions_bases.c
+++ fonctions_bases.c
@@ -40,15 +40,31 @@ bool Is_empty(int plateau[Lenght_tab][Lenght_tab], int ligne, int column){
 
 /*-------------------------------------------------------------------------*/
 
+bool Tab_is_empty(int plateau[Lenght_tab][Lenght_tab], int Indice_player, int Possible_vect[8][2]){
+    for (int init = 0; init < 8 ; init++){
+        Possible_vect[init][0] = 0;
+        Possible_vect[init][1] = 0;
+    }
+    for (int i = 0; i<8; i++){
+        for (int j = 0; j<8; j++){
+            if (Is_empty(plateau, i , j)){
+                if (Is_possible(plateau, i, j, Indice_player, Possible_vect)) return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+/*-------------------------------------------------------------------------*/
+
 bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
 
     bool possible = false;
-
+    int nb_of_possible_vec=0;
     int Vect[8][2] = {{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
 
     if (Is_empty(plateau,i,j)){
         for (int i_vect = 0; i_vect < 8; i_vect++){
-
             int pos_ligne = Vect[i_vect][0]+i;
             int pos_column = Vect[i_vect][1]+j;
 
@@ -67,15 +83,53 @@ bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_p
                     pos_column += Vect[i_vect][1];
                 }
             }
-            if ((plateau[pos_ligne][pos_column] == Indice_player) && (in_boucle==true)){
-                Possible_vect[i][0] = Vect[i][0];
-                Possible_vect[i][1] = Vect[i][1];
+            if ((plateau[pos_ligne][pos_column] == Indice_player) && (in_boucle==true) && end!=true){
+                Possible_vect[nb_of_possible_vec][0] = Vect[i_vect][0];
+                Possible_vect[nb_of_possible_vec][1] = Vect[i_vect][1];
+                nb_of_possible_vec++;
                 possible = true;
             }
         }
     }
-    if (possible) printf("Coup valide\n");
-    else printf("Coup invalide\n");
-
     return possible;
+}
+
+/*-------------------------------------------------------------------------*/
+
+void swap(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
+    int cmpt=0;
+    plateau[i][j]=  Indice_player ; 
+    while((Possible_vect[cmpt][0]!=0 || Possible_vect[cmpt][1]!=0) && cmpt < 8){
+        cmpt++;
+    }
+    for(int nb_ligne_retourne=0 ; nb_ligne_retourne < cmpt ; nb_ligne_retourne++){
+        int di = Possible_vect[nb_ligne_retourne][0];
+        int dj = Possible_vect[nb_ligne_retourne][1];
+        int pos_ligne= i+ di;
+        int pos_column = j + dj;
+        while(plateau[pos_ligne][pos_column]!= Indice_player){
+            plateau[pos_ligne][pos_column] =  Indice_player ; 
+            pos_ligne += di;
+            pos_column += dj;
+        }
+    }
+
+}
+
+/*--------------------------------------------------------------------------*/
+
+/*Retourne 0 si les pions noirs ont gagné, 1 pour les blancs et -1 sinon*/
+int Who_win(int plateau[Lenght_tab][Lenght_tab]){
+    int N = 0;
+    int B = 0;
+    int winner = -1;
+    for (int i = 0; i<Lenght_tab; i++){
+        for (int j = 0; j<Lenght_tab; j++){
+            if (plateau[i][j] == 0) N++;
+            else if (plateau[i][j] == 1) B++;
+        }
+    }
+    if (N>B) winner = 0;
+    else if (B>N) winner = 1;
+    return winner;
 }
