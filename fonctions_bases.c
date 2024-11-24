a52ef0ff70e10c5e5798fb89e5aeb0c76ebe6073
H-Boit <boitet_hugo@hotmail.com>
2024-11-24 19:13:53 +0100

    Update fonctions_bases.c

--- fonctions_bases.c
+++ fonctions_bases.c
@@ -5,8 +5,8 @@
 /*-------------------------------------------------------------------------*/
 
 void Init_tab(int plateau[Lenght_tab][Lenght_tab]) { 
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j ++){
+    for(int i = 0; i < Lenght_tab; i++){
+        for(int j = 0; j < Lenght_tab; j ++){
             plateau[i][j] = (-1);
         }
     }
@@ -18,15 +18,22 @@ void Init_tab(int plateau[Lenght_tab][Lenght_tab]) {
 
 /*-------------------------------------------------------------------------*/
 
-void Print_tab(int plateau[Lenght_tab][Lenght_tab]){ 
-    printf("  0 1 2 3 4 5 6 7");
-    printf("\n");
-    for(int i = 0; i < 8; i++){
-        printf("%d ",i);
-        for(int j = 0; j < 8; j ++){
-            if (plateau[i][j] == 0) printf("N ");
-            else if (plateau[i][j] == 1) printf("B ");
-            else printf("+ ");
+void Print_tab(int plateau[Lenght_tab][Lenght_tab]) { 
+    // Affichage des numéros des colonnes
+    printf("     0  1  2  3  4  5  6  7\n");
+
+    for (int i = 0; i < 8; i++) {
+        printf(" %d  ", i);
+        for (int j = 0; j < 8; j++) {
+
+            if ((i + j) % 2 == 0) printf(BLACK_BG);
+            else printf(WHITE_BG);
+
+            if (plateau[i][j] == -1) printf("   ");  
+            else if (plateau[i][j] == 0) printf(" B ");  
+            else if (plateau[i][j] == 1) printf(" W "); 
+
+            printf(RESET);
         }
         printf("\n");
     }
@@ -41,14 +48,14 @@ bool Is_empty(int plateau[Lenght_tab][Lenght_tab], int ligne, int column){
 /*-------------------------------------------------------------------------*/
 
 bool Tab_is_empty(int plateau[Lenght_tab][Lenght_tab], int Indice_player, int Possible_vect[8][2]){
-    for (int init = 0; init < 8 ; init++){
+    for (int init = 0; init < Lenght_tab ; init++){
         Possible_vect[init][0] = 0;
         Possible_vect[init][1] = 0;
     }
-    for (int i = 0; i<8; i++){
-        for (int j = 0; j<8; j++){
+    for (int i = 0; i<Lenght_tab; i++){
+        for (int j = 0; j<Lenght_tab; j++){
             if (Is_empty(plateau, i , j)){
-                if (Is_possible(plateau, i, j, Indice_player, Possible_vect)) return 0;
+                if (Is_possible(plateau, i, j, Indice_player, Possible_vect) > 0) return 0;
             }
         }
     }
@@ -57,43 +64,39 @@ bool Tab_is_empty(int plateau[Lenght_tab][Lenght_tab], int Indice_player, int Po
 
 /*-------------------------------------------------------------------------*/
 
-bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
+int Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
 
-    bool possible = false;
     int nb_of_possible_vec=0;
     int Vect[8][2] = {{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
 
     if (Is_empty(plateau,i,j)){
-        for (int i_vect = 0; i_vect < 8; i_vect++){
+    // iteration sur toutes les directions
+        for (int i_vect = 0; i_vect < Lenght_tab; i_vect++){
             int pos_ligne = Vect[i_vect][0]+i;
             int pos_column = Vect[i_vect][1]+j;
 
-            bool end = true;
             bool in_boucle=false;
 
-            if(pos_ligne >= 0 && pos_ligne <= 7 && pos_column >= 0 && pos_column <= 7){
+            /*
+            if((pos_ligne >= 0) && (pos_ligne < 8) && (pos_column >= 0) && (pos_column <= 7)){
                 end = false;
             }
-            while (plateau[pos_ligne][pos_column] != Indice_player && plateau[pos_ligne][pos_column] != (-1) && end == false){
-                in_boucle=true;
-                if(!(pos_ligne >= 0 && pos_ligne <= 7 && pos_column >= 0 && pos_column <= 7)){
-                    end = true;
-                } 
-                // Rajouter une condition de bordure
-                else {
-                    pos_ligne += Vect[i_vect][0];
-                    pos_column += Vect[i_vect][1];
-                }
+            */
+
+            while (plateau[pos_ligne][pos_column] == (!Indice_player) && (pos_ligne >= 0) && (pos_ligne < 8) && (pos_column >= 0) && (pos_column <= 7)){
+
+                pos_ligne += Vect[i_vect][0];
+                pos_column += Vect[i_vect][1];
+                in_boucle = true;
             }
-            if ((plateau[pos_ligne][pos_column] == Indice_player) && (in_boucle==true) && end!=true){
+            if ((plateau[pos_ligne][pos_column] == Indice_player) && (in_boucle==true) && (pos_ligne >= 0) && (pos_ligne < 8) && (pos_column >= 0) && (pos_column <= 7)){
                 Possible_vect[nb_of_possible_vec][0] = Vect[i_vect][0];
                 Possible_vect[nb_of_possible_vec][1] = Vect[i_vect][1];
-                nb_of_possible_vec++;
-                possible = true;
+                nb_of_possible_vec += 1;
             }
         }
     }
-    return possible;
+    return nb_of_possible_vec;
 }
 
 /*-------------------------------------------------------------------------*/
