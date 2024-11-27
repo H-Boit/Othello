f1fde7e60c294f104e0594e99868284478b2f374
H-Boit <boitet_hugo@hotmail.com>
2024-11-27 18:04:26 +0100

    Update IA.c

---
+++ IA.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "fonctions.h"
+
+/*----------------------------------------------------------------------------------*/
+
+int IA(int plateau[Lenght_tab][Lenght_tab], int Indice_player){
+    int Possible_vect[8][2];
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            int play=Is_possible(plateau, i, j, Indice_player, Possible_vect);
+			    if(play>0){
+                    return i*8+j;
+                }
+        }
+    }
+    return 64;
+}
+
+/*----------------------------------------------------------------------------------*/
+
+int IA_max_pts(int plateau[Lenght_tab][Lenght_tab], int Indice_player){
+    int tab_max_pts[64]; // 
+    int Possible_vect[8][2];
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            Is_possible(plateau, i, j, Indice_player, Possible_vect);
+        }
+    }
+
+    return 64;
+}
