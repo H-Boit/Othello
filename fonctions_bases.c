b0713c43f462c755a159a6d0a31e90c3d4bc1589
H-Boit <boitet_hugo@hotmail.com>
2024-11-27 18:02:52 +0100

    Update fonctions_bases.c

--- fonctions_bases.c
+++ fonctions_bases.c
@@ -63,7 +63,20 @@ bool Tab_is_empty(int plateau[Lenght_tab][Lenght_tab], int Indice_player, int Po
 }
 
 /*-------------------------------------------------------------------------*/
+/*
+    PARAMETRES :
+        plateau : le plateau de jeux, 
+        i : l'indice de ligne du coup,
+        j : l'indice de colonne du coup,
+        Indice_player : le numéro correspondant à la couleur du joueur,
+        Possible_vect : un tableau initialisé vide
+    
+    RETURN :
+        nb_of_possible_vec : nombre de vecteurs de direction
 
+    UPDATE :
+        Possible_vect : remplit les nb_of_possible_vec premières cases avec les vecteurs possibles
+*/
 int Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
 
     int nb_of_possible_vec=0;
@@ -76,12 +89,6 @@ int Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_pl
             int pos_column = Vect[i_vect][1]+j;
 
             bool in_boucle=false;
-
-            /*
-            if((pos_ligne >= 0) && (pos_ligne < 8) && (pos_column >= 0) && (pos_column <= 7)){
-                end = false;
-            }
-            */
 
             while (plateau[pos_ligne][pos_column] == (!Indice_player) && (pos_ligne >= 0) && (pos_ligne < 8) && (pos_column >= 0) && (pos_column <= 7)){
 
@@ -100,30 +107,44 @@ int Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_pl
 }
 
 /*-------------------------------------------------------------------------*/
-
-void swap(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
-    int cmpt=0;
+/*  PARAMETRES :
+        plateau : le plateau de jeux, 
+        i : l'indice de ligne du coup,
+        j : l'indice de colonne du coup,
+        Indice_player : le numéro correspondant à la couleur du joueur,
+        Possible_vect : un tableau contenant les vecteurs possibles de déplacement,
+    
+    RETURN :
+        p_ret : nombre de pions retournés
+*/
+int swap(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]){
+    int p_ret = 0, cmpt = 0, temp_p_ret = 0;
     plateau[i][j]=  Indice_player ; 
     while((Possible_vect[cmpt][0]!=0 || Possible_vect[cmpt][1]!=0) && cmpt < 8){
-        cmpt++;
-    }
-    for(int nb_ligne_retourne=0 ; nb_ligne_retourne < cmpt ; nb_ligne_retourne++){
-        int di = Possible_vect[nb_ligne_retourne][0];
-        int dj = Possible_vect[nb_ligne_retourne][1];
+        int di = Possible_vect[cmpt][0];
+        int dj = Possible_vect[cmpt][1];
         int pos_ligne= i+ di;
         int pos_column = j + dj;
         while(plateau[pos_ligne][pos_column]!= Indice_player){
             plateau[pos_ligne][pos_column] =  Indice_player ; 
             pos_ligne += di;
             pos_column += dj;
+            temp_p_ret++;
         }
+        p_ret = temp_p_ret +1;
+        temp_p_ret = 0;
+        cmpt++;
     }
-
+    return p_ret;
 }
 
 /*--------------------------------------------------------------------------*/
+/*  PARAMETRES :
+        plateau : le plateau de jeux,
 
-/*Retourne 0 si les pions noirs ont gagné, 1 pour les blancs et -1 sinon*/
+    RETURN : 
+        winner : 0 si les pions noirs ont gagné, 1 pour les blancs et -1 sinon
+*/
 int Who_win(int plateau[Lenght_tab][Lenght_tab]){
     int N = 0;
     int B = 0;
